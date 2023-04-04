bc3ec14a4778fcd022c0366f15c99c2543c6c750
Aijo Joy <joaijo93@gmail.com>
2023-04-04 17:30:03 +0530

    Review Accordion added

---
+++ moonstride-frontend/pages/components/DirectoryBase/AccordionType/AccordionTypeReview.js
@@ -0,0 +1,32 @@
+import Accordion from 'react-bootstrap/Accordion';
+import Styles from "./AccordionType.module.scss";
+var inclusionsHtml = `<ul class="aminities">`;
+const faqsList = [
+  {
+    Key: 0, 
+    header: 'See all reviews', 
+    bodycon: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    footer: 'view-more',
+  },
+];
+
+function AccordionType(props) {
+  const myLists = props.myLists;  
+  return (
+    <Accordion defaultActiveKey="0" flush className={`${Styles.accordiontype} ${props.className}`}>
+      {faqsList.map((faqlist) =>
+        // eslint-disable-next-line react/jsx-key
+        <Accordion.Item eventKey={faqlist.Key}>
+            <Accordion.Header>{faqlist.header}</Accordion.Header>
+            <Accordion.Body>{faqlist.bodycon}</Accordion.Body>
+        </Accordion.Item>
+
+
+      )}
+    </Accordion>
+  );
+}
+AccordionType.defaultProps = {
+  className:"",
+}
+export default AccordionType;
