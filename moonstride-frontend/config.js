66a17b4e2c8aa93fd34af6c39c31736a186f1f8c
hariprasad <harih79562@gmail.com>
2023-04-11 11:56:41 +0530

    new file config.js

---
+++ moonstride-frontend/config.js
@@ -0,0 +1,8 @@
+//                                                                                      //
+//                        THIS FILE IS USE TO STORE THE ESSENTIAL DATA                  //
+//                                                                                      //
+
+
+
+
+export const API_BASE_URL = 'http://192.168.200.81:4000'; 
